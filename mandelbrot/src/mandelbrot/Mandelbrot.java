3056f920c12d8c11c26fad0082de1408c2bf8b63
BenBrassard94 <62662981+BenBrassard94@users.noreply.github.com>
2020-04-13 13:35:13 -0600

    Add code from pdf

--- mandelbrot/src/mandelbrot/Mandelbrot.java
+++ mandelbrot/src/mandelbrot/Mandelbrot.java
@@ -1,14 +1,27 @@
 
 package mandelbrot;
 
-/**
- *
- * @author bj.brassard
- */
-public class Mandelbrot {
+import javax.swing.JFrame;
+
+public class Mandelbrot extends JFrame{
+    private static final int S_WIDTH = 512;
+    private static final int S_HEIGHT = 512;
+    private static final String S_TITLE = "Simple";
+    
+    public Mandelbrot(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(S_WIDTH, S_HEIGHT);
+        this.setTitle(S_TITLE);
+        
+        SimpleBitmap panel = new SimpleBitmap();
+        pane.add(panel);
+        
+        this.setVisible(true);
+        
+    } // Mandelbrot()
 
     public static void main(String[] args) {
-
-    }
+        Mandelbrot simple = new Mandelbrot();
+    } // main(String[])
     
-}
+} // Mandelbrot
